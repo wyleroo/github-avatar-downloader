e1df1c09e162a33b791723815bf7efcd44f66baf
Wyatt Robinson <thewyatt@gmail.com>
2017-03-14 04:43:44 +0000

    Added support for command line arguments and made them conditional

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 // Set globals
+
 var fs = require('fs');
 var request = require('request');
 
@@ -6,21 +7,17 @@ var GITHUB_USER = "wyleroo";
 var GITHUB_TOKEN = "94cec98e0f19c1fa5082ffee9e70e0462eeaab9e";
 var USER_AGENT = 'wyleroo';
 
-//
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-  // Authentication
+function getRepoContributors(repoOwner, repoName, cb) {
+  if (repoOwner && repoName);
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var options = {
-    url: 'https://api.github.com/repos/jquery/jquery/contributors',
+    url: requestURL,
     headers: {
-     'User-Agent': USER_AGENT,
+    'User-Agent': USER_AGENT,
     }
   };
-
-
-// Main fxn start
-function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   request(options, function (error, response, body) {
     let obj = JSON.parse(body);
     cb(null, obj);
@@ -36,10 +33,11 @@ request.get(url)
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
   result.forEach(function(element) {
     var filePath = './hotPics/' + element.login + '.jpg';
-    downloadImageByURL(element, filePath);
+    downloadImageByURL(element.avatar_url, filePath);
   });
 });
+
 
